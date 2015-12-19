5f66d4fc3b0115399b76678f94a9fe05ddd0af25
Stewart Mackenzie <setori88@gmail.com>
2015-12-20 04:33:14 +0800

    allow O(1) component lookups

--- components/mapping.nix
+++ components/mapping.nix
@@ -1,8 +1,14 @@
-{ pkgs, lib, components }:
+{ pkgs, lib, components, rustcMaster }:
 let
 mapping = pkgs.writeTextFile {
-	name = "mapping.txt";
-	text = lib.concatMapStringsSep "\n" (pkg: "${pkg.name} ${pkg.outPath}") (lib.attrValues components);
+	name = "mapping.rs";
+	text = ''
+use std::collections::HashMap;
+fn main() {
+let mut components = HashMap::with_capacity(${(builtins.toString (builtins.length (lib.attrValues components)))});
+${lib.concatMapStringsSep "\n" (pkg: "components.insert(\"${pkg.name}\", \"${pkg.outPath}\");")(lib.attrValues components)}
+}
+	'';
 	executable = false;
 };
 in
@@ -10,9 +16,12 @@ pkgs.stdenv.mkDerivation rec {
 	name = "mapping-${version}";
 	version = "2015-12-18";
 	unpackPhase = "true";
+	buildInputs = [ rustcMaster ];
 	installPhase = ''
-		mkdir -p $out/etc/
-		cp -r  ${mapping} $out/etc/mapping.txt
+	mkdir -p $out/etc
+	mkdir -p $out/bin
+	rustc --crate-type=lib ${mapping} -o $out/bin
+	cp -r  ${mapping} $out/etc/mapping.rs
 	'';
 }
 
