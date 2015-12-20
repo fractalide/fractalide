7b23c9d00e4d47b7dbd6844009668cda5ec4cffe
Stewart Mackenzie <setori88@gmail.com>
2015-12-21 04:26:02 +0800

    rm tab indentation + return immutable HashMap

--- components/mapping.nix
+++ components/mapping.nix
@@ -1,27 +1,31 @@
 { pkgs, lib, components, rustcMaster }:
 let
 mapping = pkgs.writeTextFile {
-	name = "mapping.rs";
-	text = ''
+  name = "mapping.rs";
+  text = ''
 use std::collections::HashMap;
+
 #[allow(dead_code)]
-fn components() {
-let mut components = HashMap::with_capacity(${(builtins.toString (builtins.length (lib.attrValues components)))});
-${lib.concatMapStringsSep "\n" (pkg: "components.insert(\"${pkg.name}\", \"${pkg.outPath}\");")(lib.attrValues components)}
+fn components() -> HashMap<&'static str, &'static str> {
+let mut map  = HashMap::<&str, &str>::with_capacity(${(builtins.toString (builtins.length (lib.attrValues components)))});
+${lib.concatMapStringsSep "\n" (pkg: "map.insert(\"${pkg.name}\", \"${pkg.outPath}\");")(lib.attrValues components)}
+map
 }
-	'';
-	executable = false;
+
+// caller should do this:
+// let immutable_map = components(); // without the mut it is immutable
+'';
+    executable = false;
 };
 in
 pkgs.stdenv.mkDerivation rec {
-	name = "fractalide-mappings-${version}";
-	version = "2015-12-18";
-	unpackPhase = "true";
-	buildInputs = [ rustcMaster ];
-	installPhase = ''
-	mkdir -p $out/{etc,lib}
-	cp -r  ${mapping} $out/etc/mapping.rs
-	rustc -C no-stack-check -O --crate-type=rlib $out/etc/mapping.rs --out-dir $out/lib/
-	'';
+  name = "fractalide-mappings-${version}";
+  version = "2015-12-18";
+  unpackPhase = "true";
+  buildInputs = [ rustcMaster ];
+  installPhase = ''
+  mkdir -p $out/{src,lib}
+  cp -r  ${mapping} $out/src/mapping.rs
+  rustc -C no-stack-check -O --crate-type=rlib $out/src/mapping.rs --out-dir $out/lib/
+  '';
 }
-
