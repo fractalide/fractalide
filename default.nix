85390d2f0443aad1ef2fdb78391283cc84d1cb16
Stewart Mackenzie <setori88@gmail.com>
2015-12-22 08:30:52 +0800

    Problem: libmapping.so needs cargo support for rust's fst library Solution: code write the lib.rs file and write it in during the postPatch stage.

--- default.nix
+++ default.nix
@@ -2,19 +2,19 @@
 let
 callPackage = pkgs.lib.callPackageWith (pkgs // support // components);
 support = {
-	cargo = pkgs.cargo;
-	rustcMaster = pkgs.rustcMaster;
-	rustRegistry = callPackage ./build-support/rust-packages.nix {};
-	buildFractalideComponent = callPackage ./build-support/buildFractalideComponent.nix {};
-	buildRustPackage = callPackage ./build-support/buildRustPackage.nix {};
-	capnpc-rust = callPackage ./build-support/capnpc-rust {};
+  cargo = pkgs.cargo;
+  rustcMaster = pkgs.rustcMaster;
+  rustRegistry = callPackage ./build-support/rust-packages.nix {};
+  buildFractalideComponent = callPackage ./build-support/buildFractalideComponent.nix {};
+  buildRustPackage = callPackage ./build-support/buildRustPackage.nix {};
+  capnpc-rust = callPackage ./build-support/capnpc-rust {};
 };
 components = rec {
-	not = callPackage ./components/maths/boolean/not {};
-	nand = callPackage ./components/maths/boolean/nand {};
-	add = callPackage ./components/maths/number/add {};
+  not = callPackage ./components/maths/boolean/not {};
+  nand = callPackage ./components/maths/boolean/nand {};
+  add = callPackage ./components/maths/number/add {};
 };
 in {
-	inherit support components;
-	mapping = callPackage ./components/mapping.nix { inherit components; };
+  inherit support components;
+  mapping = callPackage ./mapping { inherit components; };
 }
