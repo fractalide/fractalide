2a4f4b659bd0ae43748cadfa32f7c19e4b41f105
Stewart Mackenzie <setori88@gmail.com>
2015-12-30 02:40:23 +1100

    Problem: Nand logic gate only had 1 input Solution: add a b input + logic

--- components/maths/boolean/nand/src/lib.rs
+++ components/maths/boolean/nand/src/lib.rs
@@ -9,24 +9,28 @@ use rustfbp::component::*;
 
 component! {
   Nand,
-  inputs(input: boolean),
+  inputs(a: boolean, b: boolean),
   inputs_array(),
   outputs(output: boolean),
   outputs_array(),
   option(),
   acc(),
   fn run(&mut self) {
-    let mut ip = self.ports.recv("input".into()).expect("cannot receive");
-    let m = ip.get_reader().expect("cannot get reader");
-    let m: boolean::Reader = m.get_root().expect("not a boolean reader");
-    let n = m.get_boolean();
-    let mut new_m = super::capnp::message::Builder::new_default();
+    let mut ip_a = self.ports.recv("a".into()).expect("cannot receive");
+    let mut ip_b = self.ports.recv("b".into()).expect("cannot receive");
+    let a_reader = ip_a.get_reader().expect("cannot get reader");
+    let b_reader = ip_b.get_reader().expect("cannot get reader");
+    let a_reader: boolean::Reader = a_reader.get_root().expect("not a boolean reader");
+    let b_reader: boolean::Reader = b_reader.get_root().expect("not a boolean reader");
+    let a = a_reader.get_boolean();
+    let b = b_reader.get_boolean();
+    let mut new_out = super::capnp::message::Builder::new_default();
     {
-      let mut boolean = new_m.init_root::<boolean::Builder>();
-      boolean.set_boolean(!n);
+      let mut boolean = new_out.init_root::<boolean::Builder>();
+      boolean.set_boolean(if a == true && b == true {false} else {true});
     }
-    ip.write_builder(&new_m);
-    self.ports.send("output".into(), ip).expect("cannot send date");
+    ip_a.write_builder(&new_out);
+    self.ports.send("output".into(), ip_a).expect("cannot send date");
   }
 
   mod boolean_capnp {
