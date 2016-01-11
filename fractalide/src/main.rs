8558b2a2f061e87d603a7353d27f4a8eaac51267
Stewart Mackenzie <setori88@gmail.com>
2016-01-11 15:24:38 +0800

    Problem: fractalide service cannot parse cli args
    Solution: Use clap-rs to handle basic features.

--- fractalide/src/main.rs
+++ fractalide/src/main.rs
@@ -1,7 +1,42 @@
-#![crate_type = "bin"]
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg};
 
-extern crate fractalide;
+fn main() {
+    let matches = App::new("fractalide")
+    .author("Stewart Mackenzie <setori88@gmail.com>")
+    .version(&crate_version!()[..])
+    .about("Server of Fractalide Flow-based Programming components.")
+    .arg(Arg::with_name("host")
+        .index(1)
+        .required(true)
+        .help("host to listen on: <host>:<port> i.e. \"localhost:8000\""))
+    .arg(Arg::with_name("log")
+        .index(2)
+        .required(true)
+        .help("location of logfile, typically \"/var/fractalide/fractal.log\""))
+    .arg(Arg::with_name("debug")
+        .short("d")
+        .help("Turn on debugging."))
+    .arg(Arg::with_name("verbosity")
+        .short("v")
+        .multiple(true)
+        .help("Output verbosity level."))
+    .get_matches();
 
-pub fn main() {
-    ::fractalide::hello();
+    match matches.occurrences_of("verbosity") {
+        0 => (),
+        1 => println!("Critical debug info on"),
+        2 => println!("General debug info on"),
+        3 | _ => println!("Print everything..."),
+    }
+    if matches.is_present("debug") {
+        println!("Print something.")
+    }
+    if let Some(port) = matches.value_of("host"){
+        println!("Using host: {}", port);
+    }
+    if let Some(log) = matches.value_of("log"){
+        println!("Using logfile: {}", log);
+    }
 }
