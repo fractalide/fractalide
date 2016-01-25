5fd23ed85833f70376e7c905cbc642a1da5e0efc
Stewart Mackenzie <setori88@gmail.com>
2016-01-25 19:47:43 +0800

    Problem: No manual for programmers.
    Solution: start one

---
+++ doc/default.nix
@@ -0,0 +1,62 @@
+{ pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv, lib ? pkgs.lib }:
+with lib;
+let
+  sources = sourceFilesBySuffices ./. [".xml"];
+
+in
+stdenv.mkDerivation {
+  name = "fractalide-manual";
+
+
+  buildInputs = [ pkgs.pandoc pkgs.libxml2 pkgs.libxslt ];
+
+  xsltFlags = ''
+    --param section.autolabel 1
+    --param section.label.includes.component.label 1
+    --param html.stylesheet 'style.css'
+    --param xref.with.number.and.title 1
+    --param toc.section.depth 3
+    --param admon.style '''
+    --param callout.graphics.extension '.gif'
+  '';
+
+
+  buildCommand = let toDocbook = { useChapters ? false, inputFile, outputFile }:
+    let
+      extraHeader = ''xmlns="http://docbook.org/ns/docbook" xmlns:xlink="http://www.w3.org/1999/xlink" '';
+    in ''
+      {
+        pandoc '${inputFile}' -w docbook ${optionalString useChapters "--chapters"} \
+          | sed -e 's|<ulink url=|<link xlink:href=|' \
+              -e 's|</ulink>|</link>|' \
+              -e 's|<sect. id=|<section xml:id=|' \
+              -e 's|</sect[0-9]>|</section>|' \
+              -e '1s| id=| xml:id=|' \
+              -e '1s|\(<[^ ]* \)|\1${extraHeader}|'
+      } > '${outputFile}'
+    '';
+  in
+
+  ''
+    ln -s '${sources}/'*.xml .
+
+    xmllint --noout --nonet --xinclude --noxincludenode \
+      --relaxng ${pkgs.docbook5}/xml/rng/docbook/docbook.rng \
+      manual.xml
+
+    dst=$out/share/doc/fractalide
+    mkdir -p $dst
+    xsltproc $xsltFlags --nonet --xinclude \
+      --output $dst/manual.html \
+      ${pkgs.docbook5_xsl}/xml/xsl/docbook/xhtml/docbook.xsl \
+      ./manual.xml
+
+    cp ${./style.css} $dst/style.css
+
+    mkdir -p $dst/images/callouts
+    cp "${pkgs.docbook5_xsl}/xml/xsl/docbook/images/callouts/"*.gif $dst/images/callouts/
+
+    mkdir -p $out/nix-support
+    echo "doc manual $dst manual.html" >> $out/nix-support/hydra-build-products
+  '';
+}
