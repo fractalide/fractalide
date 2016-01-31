929c7125a99e491e6a353fe0695da27d95072e42
Stewart Mackenzie <setori88@gmail.com>
2016-02-01 02:42:57 +0800

    Problem: apk-build was moved to build-support
    Solution: reference apk-build via attribute and not path

--- fvm/fvm-android/default.nix
+++ fvm/fvm-android/default.nix
@@ -1,23 +1,20 @@
 {pkgs
   , stdenv ? pkgs.stdenv
-  , ndk-standalone-toolchain ? support.ndk-standalone-toolchain
-  , rustcMaster ? support.rustcMaster
-  , llvmPackages_37 ? pkgs.llvmPackages_37
   , support}:
 
   let
-  llvmShared = llvmPackages_37.llvm.override { enableSharedLibraries = true; };
+  apk-build = support.apk-builder;
+  llvmShared = pkgs.llvmPackages_37.llvm.override { enableSharedLibraries = true; };
   forceBundledLLVM = true;
-  rust = rustcMaster.overrideDerivation (oldAttrs: {
+  rust = support.rustcMaster.overrideDerivation (oldAttrs: {
     forceBundledLLVM = forceBundledLLVM;
     configureFlags =
-    ["--target=arm-linux-androideabi --arm-linux-androideabi-ndk=${ndk-standalone-toolchain}"] ++
+    ["--target=arm-linux-androideabi --arm-linux-androideabi-ndk=${support.ndk-standalone-toolchain}"] ++
     [ "--enable-local-rust" "--local-rust-root=$snapshot" "--enable-rpath" ]
     ++ [ "--release-channel=stable" ]
     ++ [ "--default-linker=${stdenv.cc}/bin/cc" "--default-ar=${stdenv.cc.binutils}/bin/ar" ]
     ++ stdenv.lib.optional (stdenv.cc.cc ? isClang) "--enable-clang"
     ++ stdenv.lib.optional (!forceBundledLLVM) "--llvm-root=${llvmShared}";
     });
-  apk-build = import ./apk-builder.nix {};
   in
   rust
