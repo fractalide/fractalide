76bf127c9ce7d8ef445b92f30aae3367b545b6a2
Stewart Mackenzie <setori88@gmail.com>
2016-02-05 04:13:04 +0800

    Problem: 'pass-remarks' registered more than once!
    Solution: use configureFlagsArray and a few other changes
    
    Thanks Tuomas Tynkkynen!

--- build-support/rust-android.nix
+++ build-support/rust-android.nix
@@ -1,22 +1,41 @@
 {pkgs
+  , fetchurl
   , stdenv ? pkgs.stdenv
-  , apk-builder
   , rustcMaster
   , ndk-standalone-toolchain
   }:
 
+# https://ghotiphud.github.io/rust/android/cross-compiling/2016/01/06/compiling-rust-to-android.html
+# https://github.com/japaric/ruststrap/blob/master/1-how-to-cross-compile.md
+# https://github.com/rust-lang/rust-wiki-backup/blob/master/Doc-building-for-android.md
+
   let
-  apk-build = apk-builder;
-  llvmShared = import <nixpkgs/pkgs/development/compilers/llvm/3.7/default.nix> {enableSharedLibraries = true; };
+  stdenv_32bit = pkgs.pkgsi686Linux.stdenv;
+  zlib_32bit = pkgs.pkgsi686Linux.zlib;
+  ncurses_32bit = pkgs.pkgsi686Linux.ncurses;
+  llvmShared = pkgs.llvmPackages_37.llvm.override { enableSharedLibraries = true; };
   forceBundledLLVM = true;
   rust = rustcMaster.overrideDerivation (oldAttrs: {
     forceBundledLLVM = forceBundledLLVM;
-    configureFlags =
-    ["--target=arm-linux-androideabi --arm-linux-androideabi-ndk=${ndk-standalone-toolchain}"] ++
-    [ "--enable-local-rust" "--local-rust-root=$snapshot" "--enable-rpath" ]
-    ++ [ "--default-linker=${stdenv.cc}/bin/cc" "--default-ar=${stdenv.cc.binutils}/bin/ar" ]
-    ++ stdenv.lib.optional (stdenv.cc.cc ? isClang) "--enable-clang"
-    ++ stdenv.lib.optional (!forceBundledLLVM) "--llvm-root=${llvmShared}";
+    nativeBuildInputs = with pkgs; [ file python2 procps ncurses_32bit  zlib_32bit stdenv_32bit];
+    preConfigure = ''
+        configureFlagsArray+=("--target=arm-linux-androideabi")
+        configureFlagsArray+=("--arm-linux-androideabi-ndk=${ndk-standalone-toolchain}")
+        configureFlagsArray+=("--host=x86_64-unknown-linux-gnu")
+        configureFlagsArray+=("--enable-local-rust")
+        configureFlagsArray+=("--jemalloc-root=${pkgs.jemalloc}/lib")
+        configureFlagsArray+=("--local-rust-root=$snapshot")
+        configureFlagsArray+=("--enable-rpath")
+        configureFlagsArray+=("--release-channel=stable")
+        configureFlagsArray+=("--default-linker=${pkgs.pkgsi686Linux.stdenv.cc}/bin/cc")
+        configureFlagsArray+=("--default-ar=${pkgs.pkgsi686Linux.stdenv.cc.binutils}/bin/ar")
+        configureFlagsArray+=("--enable-clang")
+        configureFlagsArray+=("--llvm-root=${llvmShared}")
+        configureFlagsArray+=("--android-cross-path=${ndk-standalone-toolchain}")
+        configureFlagsArray+=("--datadir=$out/share")
+        configureFlagsArray+=("--infodir=$out/share/info")
+        '';
+
     });
   in
   rust
