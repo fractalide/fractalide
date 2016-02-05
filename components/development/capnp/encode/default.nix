a2bfaac321504d11cc8e7f5b454b72c0a3d03bad
Denis Michiels <dmichiels@gmail.com>
2016-02-05 16:11:11 +0100

    [FVM] Build and send IIP from .fbp
    The fvm now build IIP and send them to the components.
    The iip had the following format :
    ```
    'contract_name:(field=value)' -> input ...
    ```
    
    IE :
    ```
    'path:(path="~/test.fbp")' -> input open(file_open)
    'maths_boolean:(boolean=true)' -> input print(maths_boolean_print)
    ```

---
+++ components/development/capnp/encode/default.nix
@@ -0,0 +1,15 @@
+{ stdenv, buildFractalideComponent, filterContracts, genName, upkeepers, ...}:
+
+buildFractalideComponent rec {
+  name = genName ./.;
+  src = ./.;
+  filteredContracts = filterContracts ["generic_text" "path"];
+  depsSha256 = "1g35gpls317czczzv2xa5plbzvn9809i5xvx53r46qd87pq94yvx";
+
+  meta = with stdenv.lib; {
+    description = "Component: Fractalide Virtual Machine";
+    homepage = https://github.com/fractalide/fractalide/tree/master/components/development/fvm;
+    license = with licenses; [ mpl20 ];
+    maintainers = with upkeepers; [ dmichiels ];
+  };
+}
