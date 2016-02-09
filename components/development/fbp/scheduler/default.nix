f578276355b3aa9f6e86460cf2613ad0788836d9
Stewart Mackenzie <setori88@gmail.com>
2016-02-09 21:56:09 +0800

    Problem: Contract info missing from docs
    Solution: add it

--- components/development/fbp/scheduler/default.nix
+++ components/development/fbp/scheduler/default.nix
@@ -3,7 +3,7 @@
 buildFractalideComponent rec {
   name = genName ./.;
   src = ./.;
-  filteredContracts = filterContracts ["fbp_graph" "maths_boolean" "path" "generic_text"];
+  filteredContracts = filterContracts ["fbp_graph" "path" "generic_text"];
   depsSha256 = "11mpqfda6byhssfcq7cbk1qh1xjknh5qpxxma59m05ljw5165zi3";
 
   meta = with stdenv.lib; {
