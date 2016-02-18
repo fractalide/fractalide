cf4989ba60835cb8517c05262666e47435489019
Stewart Mackenzie <setori88@gmail.com>
2016-02-18 16:20:24 +0800

    Problem: manual doesn't reflect current system
    Solution: make it reflect it

--- components/development/test/default.nix
+++ components/development/test/default.nix
@@ -1,23 +1,12 @@
 { stdenv, buildFractalideSubnet, upkeepers
-  , web_server
   , maths_boolean_not
-  , maths_boolean_and
   , maths_boolean_print
   , ...}:
 
-let
-doc = import ../../../doc {};
-in
 buildFractalideSubnet rec {
   src = ./.;
   subnet = ''
-'path:(path="${doc}/share/doc/fractalide/manual.html")' -> www_dir www(${web_server})
-'domain_port:(domainPort="localhost:8080")' -> domain_port www()
-'url:(url="/docs")' -> url www()
-
 'maths_boolean:(boolean=true)' -> input not(${maths_boolean_not}) output -> input disp(${maths_boolean_print})
-'maths_boolean:(boolean=true)' -> a and(${maths_boolean_and}) output -> input disp()
-'maths_boolean:(boolean=true)' -> b and()
   '';
 
   meta = with stdenv.lib; {
