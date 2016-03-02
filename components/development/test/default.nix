4ce51c6139de22d5695071e331b5064a1945c104
Stewart Mackenzie <setori88@gmail.com>
2016-03-03 02:46:19 +0800

    Problem: not needed deps
    Solution: remove them

--- components/development/test/default.nix
+++ components/development/test/default.nix
@@ -8,21 +8,12 @@
   , web_server
   , io_print
   , ui_magic
-  , freetype
-  , SDL2
-  , mesa
-  , xlibs
   , ...}:
   let
   doc = import ../../../doc {};
   in
   buildFractalideSubnet rec {
-
    src = ./.;
-
-   buildInputs = [ freetype SDL2 ];
-   LD_LIBRARY_PATH = with xlibs; "${mesa}/lib:${libX11}/lib:${libXcursor}/lib:${libXxf86vm}/lib:${libXi}/lib:${SDL2}/lib";
-
    subnet = ''
    'maths_boolean:(boolean=false)' -> input not(${maths_boolean_not}) output -> input disp(${maths_boolean_print})
    'maths_boolean:(boolean=false)' -> a nand(${maths_boolean_nand}) output -> input disp(${maths_boolean_print})
@@ -33,13 +24,10 @@
 
    button() output[button_clicked] -> input print_clicked(${print})
    'generic_text:(text="A button is clicked")' -> option print_clicked()
-
    'path:(path="${doc}/share/doc/fractalide/")' -> www_dir www(${web_server})
    'domain_port:(domainPort="localhost:8083")' -> domain_port www()
    'url:(url="/docs")' -> url www()
    'generic_text:(text="[*] serving: localhost:8083/docs/manual.html")' -> input disp(${io_print})
-
-
 
    'maths_boolean:(boolean=true)' -> input window(${ui_conrod_window})
    '';
