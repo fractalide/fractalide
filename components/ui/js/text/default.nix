89ba640817988356131fe627e3f39c6eb5396a68
Michiels Denis <dmichiels@mailoo.org>
2016-05-04 17:08:18 +0200

    Add the Text widget

---
+++ components/ui/js/text/default.nix
@@ -0,0 +1,15 @@
+{ stdenv, buildFractalideComponent, filterContracts, genName, upkeepers, ...}:
+
+buildFractalideComponent rec {
+  name = genName ./.;
+  src = ./.;
+  filteredContracts = filterContracts ["js_text" "js_create" "generic_text"];
+  depsSha256 = "01062z2pzhd1jm5vag6nw17cdjk78163jq62b91c9cg6cj4sx6bk";
+
+  meta = with stdenv.lib; {
+    description = "Component: draw a conrod button";
+    homepage = https://github.com/fractalide/fractalide/tree/master/components/maths/boolean/print;
+    license = with licenses; [ mpl20 ];
+    maintainers = with upkeepers; [ dmichiels sjmackenzie];
+  };
+}
