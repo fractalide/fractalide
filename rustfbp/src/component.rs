bd9cfc8f17dfe84dbaff99b58e1c33ac079c48b1
Stewart Mackenzie <setori88@gmail.com>
2016-05-25 20:04:51 +0800

    Problem: rustfbp topical refactor
    Solution:
    * remove LICENSE as its covered by the root license
    * removed useless bin directory
    * published rustfbp with a version bump from 0.3.11 -> 0.3.12

--- rustfbp/src/component.rs
+++ rustfbp/src/component.rs
@@ -1,23 +1,3 @@
-/*
- * The component library for rustfbp
- *
- * Author : Denis Michiels
- * Copyright (C) 2015 Michiels Denis
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
-
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 extern crate capnp;
 use ports::Ports;
 use result::Result;
