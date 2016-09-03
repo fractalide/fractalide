8b9f8a69e7f5ab33e2ee0b96178ef876c43bbdfe
Stewart Mackenzie <setori88@gmail.com>
2016-09-03 09:40:39 +0800

    Problem: nucleus_find_component cannot build components
    Solution: std::process:Command builds names if name is
    not a path in the /nix/store

--- components/nucleus/find/component/src/lib.rs
+++ components/nucleus/find/component/src/lib.rs
@@ -3,6 +3,8 @@ extern crate rustfbp;
 extern crate capnp;
 
 use std::fs;
+use std::str;
+use std::process::Output;
 use std::process::Command;
 
 component! {
@@ -17,11 +19,12 @@ component! {
         let mut ip = try!(self.ports.recv("input"));
         let name: path::Reader = try!(ip.get_root());
         let is_path = try!(name.get_path());
+        let mut stdout: String = String::new();
         let new_path = if fs::metadata(format!("{}", is_path)).is_ok() {
             Some(is_path)
         } else {
-            Some(is_path)
-            //lookup_path(is_path)
+            stdout = build_component(is_path);
+            Some(stdout.as_str())
         };
         let mut new_ip = IP::new();
         {
@@ -31,31 +34,25 @@ component! {
                 Some(p) => { ip.set_path(p);}
             };
         }
-        let _ = self.ports.send("output", new_ip);
+        self.ports.send("output", new_ip);
         Ok(())
     }
-
 }
 
-// fn build_component(name: &str) //-> Option<&str>
-// {
-//     //let output = Command::new(format!("nix-build --argstr debug true --argstr cache $(./support/buildCache.sh) -I nixpkgs=/home/stewart/dev/fractalide/nixpkgs/ --argstr subnet {}", name))
-//     let output = Command::new("nix-build")
-//                      .arg("--argstr debug true")
-//                      .arg("--argstr cache $(./support/buildCache.sh)")
-//                      .arg("-I nixpkgs=/home/stewart/dev/fractalide/nixpkgs/")
-//                      .arg(format!("--argstr subnet {}", name))
-//                       .output()
-//                      .expect("failed to execute process");
-//
-//     println!("status: {}", output.status);
-//     println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-//     println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-// }
+fn build_component(name: &str) -> String {
+    let output = Command::new("nix-build")
+                            .args(&["--argstr", "debug", "true"
+                            , "--argstr", "cache", "$(./support/buildCache.sh)"
+                            , "-I", "nixpkgs=/home/stewart/dev/fractalide/nixpkgs/"
+                            , "--argstr", "subnet", format!("{}", name).as_str()])
+                            .output()
+                            .expect("failed to execute process");
 
-// fn lookup_path(name: &str) -> Option<&str> {
-//     match name {
-//         nix-replace-me
-//         _ => None,
-//     }
-// }
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    match String::from_utf8(output.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Name of component contains invalid UTF-8 characters: {}", e),
+    }
+}
