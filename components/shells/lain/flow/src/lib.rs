bcb3139e8d9ca22a82fb10ec7f0820d16708075a
Stewart Mackenzie <setori88@gmail.com>
2016-09-05 17:38:57 +0800

    Problem: Lain didn't generate flowscript
    Solution: flowscript is now generated correctly based on
    
    * correctly handles 1 command
    * correctly handles more than 1 piped command
    * correctly handles the last element

--- components/shells/lain/flow/src/lib.rs
+++ components/shells/lain/flow/src/lib.rs
@@ -13,15 +13,31 @@ component! {
     acc(),
     fn run(&mut self) -> Result<()> {
         let mut ip_input = self.ports.recv("input")?;
-        let input = {
-            let input_reader: list_text::Reader = ip_input.get_root()?;
-            input_reader.get_texts()
-        };
-
+        let mut flow = String::from("");
+        let input_reader: list_text::Reader = ip_input.get_root()?;
+        let mut cmd_iterator = input_reader.get_texts()?;
+        let mut count = cmd_iterator.iter().count();
+        let mut i :usize = 0;
+        for cmd in cmd_iterator.iter() {
+            if count == 1 {
+                flow.push_str(format!("{}({})", cmd?, cmd?).as_str());
+            } else {
+                if count > 1 && i == 0 {
+                    flow.push_str(format!("{}({}) output -> ", cmd?, cmd?).as_str());
+                } else {
+                    if (count - 1) == i {
+                        flow.push_str(format!("input {}({})", cmd?, cmd?).as_str());
+                    } else {
+                        flow.push_str(format!("input {}({}) output -> ", cmd?, cmd?).as_str());
+                    }
+                }
+            }
+            i += 1;
+        }
         let mut out_ip_output = IP::new();
         {
-          let mut variable = out_ip_output.init_root::<generic_text::Builder>();
-          variable.set_text("generated nix subnet code!");
+            let mut variable = out_ip_output.init_root::<generic_text::Builder>();
+            variable.set_text(flow.as_str());
         }
         self.ports.send("output", out_ip_output)?;
         Ok(())
