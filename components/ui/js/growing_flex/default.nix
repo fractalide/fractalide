4c2ee3a8acdd6227fc34eded938c3ff205c055d0
Stewart Mackenzie <setori88@gmail.com>
2016-10-18 12:05:43 +0800

    Problem: a few sha256 are incorrect
    Solution: correct them

--- components/ui/js/growing_flex/default.nix
+++ components/ui/js/growing_flex/default.nix
@@ -9,7 +9,7 @@ buildFractalideComponent rec {
   name = genName ./.;
   src = ./.;
   contracts = [ js_create generic_text fbp_action ];
-  depsSha256 = "0dqwqi7jxnnhk1rj829f45h6v4j3sqgs6cyjzaxhmgd6miv5jg0g";
+  depsSha256 = "07sj3sh2d8nqp1x3sn4hhbfbckw2mly3k8fjxip3hdf7rr8zb0l4";
   configurePhase = ''
       substituteInPlace src/lib.rs --replace "ui_js_flex" "${ui_js_flex}"
   '';
