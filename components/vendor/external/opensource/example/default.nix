9218fa8c7b518510fd99dd2baba9f3da51c2781b
Stewart Mackenzie <setori88@gmail.com>
2016-10-23 22:26:26 +0800

    Problem: fractalide calling specialization cannot find subnet
    Solution: explicitly pass in `fractalide = null` which then
    correctly activates the vm or subnet switch in the specialization repo

--- components/vendor/external/opensource/example/default.nix
+++ components/vendor/external/opensource/example/default.nix
@@ -8,13 +8,15 @@
   , maths_boolean
   , ...}:
   let
-  repo = https://github.com/fractalide/fractalide_external_opensource_example/archive/c94d812f5acb0f9317d62f51cd7363c3f4deb2f0.tar.gz;
-  external_opensource_nand_gate = import (fetchTarball repo)  {inherit pkgs support contracts components;};
+  repo = https://github.com/fractalide/fractalide_external_opensource_example/archive/ef72b8142f8e166cc27365bc0a3ad8a5a143acdf.tar.gz;
+  external_opensource_nand_gate = import (fetchTarball repo)  {inherit pkgs support contracts components; fractalide = null;};
+  /*repo = ../../../../../../fractalide_external_opensource_example;
+  external_opensource_nand_gate = import repo {inherit pkgs support contracts components; fractalide = null;};*/
   in
   buildFractalideSubnet rec {
     src = ./.;
     subnet = ''
-    '${maths_boolean}:(boolean=true)' -> a nand(external_opensource_nand_gate:${keybase_user}) output -> input io_print(${maths_boolean_print})
+    '${maths_boolean}:(boolean=true)' -> a nand(${external_opensource_nand_gate}) output -> input io_print(${maths_boolean_print})
     '${maths_boolean}:(boolean=true)' -> b nand()
     '';
 
