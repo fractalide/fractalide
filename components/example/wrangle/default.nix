1266854bcdc0a2ec92a4d14b622cf65c9c6ec41f
Stewart Mackenzie <setori88@gmail.com>
2016-10-24 23:03:09 +0800

    Problem: Hydra doesn't like fetchTarball
    Solution: remove it

--- components/example/wrangle/default.nix
+++ components/example/wrangle/default.nix
@@ -2,11 +2,17 @@
   , support
   , contracts
   , components
+  , fetchFromGitHub
   , ...}:
   let
-  repo = https://github.com/fractalide/frac_example_wrangle/archive/2c62da95fd45bc186d428c648ed8c5b4e4036590.tar.gz;
-  example_wrangle = import (fetchTarball repo)  {inherit pkgs support contracts components; fractalide = null;};
+  repo = fetchFromGitHub {
+      owner = "fractalide";
+      repo = "frac_example_wrangle";
+      rev = "2c62da95fd45bc186d428c648ed8c5b4e4036590";
+      sha256 = "0i2a8r7ww1k9ir29cgv69i4076s1h5xc4d617i32q5p8sxhj5h36";
+    };
+  example_wrangle = import repo {inherit pkgs support contracts components; fractalide = null;};
   /*repo = ../../../../frac_example_wrangle;
-  example_wrangle = import repo {inherit pkgs support contracts components; fractalide = null;};*/
+  /*example_wrangle = import repo {inherit pkgs support contracts components; fractalide = null;};*/
   in
   example_wrangle
