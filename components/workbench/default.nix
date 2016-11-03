5d52735038232652175f2a7d94ba028a29ba5efa
Michiels Denis <dmichiels@mailoo.org>
2016-11-03 11:38:23 +0100

    [Ports] some argument needs 'static lifetime
    in ports.rs, some arguments in some methods need &'static lifetime. It
    is not necessary, any lifetime is OK.

--- components/workbench/default.nix
+++ components/workbench/default.nix
@@ -7,28 +7,18 @@
   , fetchFromGitHub
   , maths_boolean_print
   , maths_boolean
+  , generic_i64
   , ...}:
   let
+  /*
   repo = fetchFromGitHub {
       owner = "fractalide";
       repo = "frac_workbench";
       rev = "28342ea221e4e4cf0d7ee751833d4e824a95660f";
       sha256 = "1hwrpbikfrlql91j137kdhb0n8z73zlsc8mrmrc4fhhwff8vf1lz";
     };
-  /*repo = ../../../../frac_example_workspace;*/
-  external_nand_gate = import repo {inherit pkgs support contracts components; fractalide = null;};
+    */
+  repo = /home/denis/dev/frac/frac_net_hyper;
+  external_net_hyper = import repo {inherit pkgs support contracts components; fractalide = null;};
   in
-  buildFractalideSubnet rec {
-    src = ./.;
-    subnet = ''
-    '${maths_boolean}:(boolean=true)' -> a nand(${external_nand_gate}) output -> input io_print(${maths_boolean_print})
-    '${maths_boolean}:(boolean=true)' -> b nand()
-    '';
-
-    meta = with stdenv.lib; {
-      description = "Subnet: testing file for sjm";
-      homepage = https://github.com/fractalide/fractalide/tree/master/components/test/sjm;
-      license = with licenses; [ mpl20 ];
-      maintainers = with upkeepers; [ dmichiels sjmackenzie];
-    };
-  }
+  external_net_hyper
