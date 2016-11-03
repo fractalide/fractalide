34cdbec4e0ac65829b7aa5ba067608c8ec16f0e5
Michiels Denis <dmichiels@mailoo.org>
2016-11-03 16:02:16 +0100

    Set the correct link

--- components/net/http/default.nix
+++ components/net/http/default.nix
@@ -2,15 +2,17 @@
   , support
   , contracts
   , components
+  , stdenv
+  , buildFractalideSubnet
+  , fetchFromGitHub
   , ...}:
   let
-  /*repo = fetchFromGitHub {
-      owner = "fractalide";
+  repo = fetchFromGitHub {
+      owner = "dmichiels";
       repo = "frac_net_http";
-      rev = "d8fdc3869e50cfe413c73b57425cc3de84cb87dc";
-      sha256 = "2l2nyx931vnyq1hdni6ny63zw5mk6yw56l3ppgkfgjrvhwviw40f";
-    };*/
-  repo = ../../../../frac_net_http;
-  net_http = import repo {inherit pkgs support contracts components; fractalide = null;};
+      rev = "6b38ded9fafaa3adbd73190e3e9233ca7070ab4a";
+      sha256 = "1yvim78ivm9cbvbvd2vg9s1b4q3ccp6is34h2a592z32gzsvwvf9";
+    };
+  external_net_http = import repo {inherit pkgs support contracts components; fractalide = null;};
   in
-  net_http
+  external_net_http
