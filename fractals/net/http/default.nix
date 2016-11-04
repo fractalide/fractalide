a27b1b6ef78c86fad2adef6b8963e6a5d2572cd9
Stewart Mackenzie <setori88@gmail.com>
2016-11-04 16:20:27 +0800

    Problem: import fractal_net_http
    Solution: not frac_net_http... the name fractal is better than
    frac

--- fractals/net/http/default.nix
+++ fractals/net/http/default.nix
@@ -5,14 +5,14 @@
   , fetchFromGitHub
   , ...}:
   let
-  /*repo = fetchFromGitHub {
+  repo = fetchFromGitHub {
     owner = "fractalide";
     repo = "fractal_net_http";
-    rev = "6bb7246d18d420b57a7c8e1f67bd7bafbfb7b19f";
-    sha256 = "14z603f2g2niphsqhclnzkr7i6nx8f3db1dci9h7vy5dq5fmb27j";
-  };*/
+    rev = "bb5e7c1f0883d467c6df7b1f4169b3af71b594e0";
+    sha256 = "1vs1d3d9lbxnyilx8g45pb01z5cl2z3gy4035h24p28p9v94jx1b";
+  };
 
-  repo = ../../../../fractals/fractal_net_http;
+  /*repo = ../../../../fractals/fractal_net_http;*/
 
   net_http = import repo {inherit pkgs support contracts components; fractalide = null;};
   in
