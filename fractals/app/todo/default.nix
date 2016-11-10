5f3a68352f113926799dca448af6dbd47f71c429
Stewart Mackenzie <setori88@gmail.com>
2016-11-10 23:22:22 +0800

    Problem: *.swp files present in commit history of fractals
    Solution: git ignore them

--- fractals/app/todo/default.nix
+++ fractals/app/todo/default.nix
@@ -5,17 +5,12 @@
   , fetchFromGitHub
   , ...}:
   let
-  repo = fetchFromGitHub {
+  fractal = fetchFromGitHub {
     owner = "fractalide";
     repo = "fractal_app_todo";
     rev = "d9e9624168a196cfb326e976fbda5324c6230557";
     sha256 = "0m4vbgk5gakl7a86ydzfqcf66w84mrvwmzxwgvbwnjbvhn056db4";
   };
-
-  /*
-  repo = ../../../../fractals/fractal_app_todo_backend;
-  */
-
-  app_todo = import repo {inherit pkgs support contracts components; fractalide = null;};
+  /*fractal = ../../../../fractals/fractal_app_todo;*/
   in
-  app_todo
+  import fractal {inherit pkgs support contracts components; fractalide = null;}
