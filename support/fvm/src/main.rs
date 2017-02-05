0bcbda106d0ff48504ba126cd108741d50e5dec8
Stewart Mackenzie <setori88@gmail.com>
2017-02-05 11:02:22 +0800

    Problem: fvm/src/main.rs is hard to understand flow
    Solution: make fbp flow comments

--- support/fvm/src/main.rs
+++ support/fvm/src/main.rs
@@ -37,17 +37,26 @@ fn run(path_fbp: &str) {
     sched.add_node("core_find_edge", "core_find_edge.so").expect("cannot add node");
     sched.add_node("core_find_node", "core_find_node.so").expect("cannot add node");
 
+    // open(fs_file_open) output -> input lex(core_parser_lexical)
+    // lex() output -> input sem(core_parser_semantic)
+    // sem() output -> input graph_check(core_parser_graph_check)
+    // graph_check() output -> input vm(core_vm)
+    // graph_check() error -> semantic_error errors(core_errors)
     sched.connect("open", "output", "lex", "input").expect("cannot connect");
     sched.connect("lex", "output", "sem", "input").expect("cannot connect");
     sched.connect("sem", "output", "graph_check", "input").expect("cannot connect");
     sched.connect("graph_check", "output", "vm", "input").expect("cannot connect");
     sched.connect("graph_check", "error", "errors", "semantic_error").expect("cannot connect");
 
+    // open() error -> file_error errors()
+    // sem() error -> semantic_error errors()
+    // errors() output -> input vm()
     sched.connect("open", "error", "errors", "file_error").expect("cannot connect");
     sched.connect("sem", "error", "errors", "semantic_error").expect("cannot connect");
     sched.connect("errors", "output", "vm", "input").expect("cannot connect");
 
     // reccursive part
+    // vm() ask_graph -> input open()
     sched.connect("vm", "ask_graph", "open", "input").expect("cannot connect");
 
     // With Graph print
@@ -55,20 +64,27 @@ fn run(path_fbp: &str) {
     // sched.connect("graph_print", "output", "sched", "graph").expect("cannot connect");
 
     // Without Graph print
+    // vm() output -> graph sched(core_scheduler)
+    // sched() ask_path -> input core_find_edge(core_find_edge)
+    // core_find_edge() output -> edge_path sched()
+    // vm() ask_path -> input core_find_node()
+    // core_find_node() output -> new_path vm()
     sched.connect("vm", "output", "sched", "graph").expect("cannot connect");
-
     sched.connect("sched", "ask_path", "core_find_edge", "input").expect("cannot connect");
     sched.connect("core_find_edge", "output", "sched", "edge_path").expect("cannot connect");
-
     sched.connect("vm", "ask_path", "core_find_node", "input").expect("cannot connect");
     sched.connect("core_find_node", "output", "vm", "new_path").expect("cannot connect");
 
     // IMsg part
+    // sched() imsg_path -> path imsg()
+    // sched() imsg_edge -> edge imsg()
+    // sched() imsg_input -> input imsg()
+    // imsg() output -> imsg sched()
+    // imsg() ask_graph -> input vm()
     sched.connect("sched", "imsg_path", "imsg", "path").expect("cannot connect");
     sched.connect("sched", "imsg_edge", "imsg", "edge").expect("cannot connect");
     sched.connect("sched", "imsg_input", "imsg", "input").expect("cannot connect");
     sched.connect("imsg", "output", "sched", "imsg").expect("cannot connect");
-
     sched.connect("sched", "ask_graph", "vm", "input").expect("cannot connect ask_graph");
 
     let add = sched.get_sender("sched", "action").expect("action of sched not found");
