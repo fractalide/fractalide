3d55e7ec5b8109217e12913c6850fd5273484030
Stewart Mackenzie <setori88@gmail.com>
2017-09-10 04:44:18 +0800

    Problem: mods/rs/crates/update.sh called non-existent binary
    Solution: call the correctly name binary `cargo2nix`

--- modules/rs/crates/update.sh
+++ modules/rs/crates/update.sh
@@ -7,8 +7,8 @@ cd -
 echo Generating lockfile
 cargo generate-lockfile
 echo Running Cargo2nix
-../cargo2nix/target/debug/generate-nix-pkg Cargo.lock -o default.nix -i -m &&
+../cargo2nix/target/debug/cargo2nix Cargo.lock -o default.nix -i -m &&
 echo Done
 echo "There's a bug in cargo2nix please manually check that all build_dependencies don't resolve to an undefined nix closure.
 For example if you search for winapi_build_0_0_0 this should be changed to winapi_build_0_1_1_
-Please make a pull request to resolve this issue in cargo2nix :-)"
+Please make a pull request to resolve this issue in cargo2nix."
