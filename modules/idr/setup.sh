9043c45c32e2db2807dca33f0af33ad3c32b9781
Stewart Mackenzie <setori88@gmail.com>
2017-10-17 03:41:07 +0800

    Problem: Control.ST stdlib not found when in repl/editor mode
    Solution: alias `idris` to `idris -i path/to/contrib -i path/to/base
    -i path/to/top-level-00*` files. This composes all transitive
    deps for idris compiler without the programmer having to worry about
    it.

--- modules/idr/setup.sh
+++ modules/idr/setup.sh
@@ -3,6 +3,8 @@ for p in $baseInputs; do
   export PATH=$p/bin${PATH:+:}$PATH
 done
 
+IDRIS_I_PATHS=()
+
 function preHook() {
   idris_version=$(idris --version)
   IDRIS_LIBRARY_PATH=$PWD/idris_libs
@@ -13,6 +15,8 @@ function preHook() {
 
   for ipkg in $propagatedBuildInputs; do
     ln -sf "$ipkg/lib/$idris_version"/* $IDRIS_LIBRARY_PATH
+    ipkg_name=$(cat "$ipkg/lib/$idris_version/NAME")
+    IDRIS_I_PATHS+=("$ipkg/lib/$idris_version/$ipkg_name")
   done
 }
 
@@ -79,6 +83,8 @@ function installPhase {
     cp fvm $out/
   else
     idris --install *.ipkg --ibcsubdir $IBCSUBDIR
+    ipkg_name=($(grep -oP '^\s*package\s+([a-zA-Z][-_A-Za-z0-9]*)' $(echo *.ipkg)))
+    echo "${ipkg_name[1]}" >> $IBCSUBDIR/NAME
   fi
 }
 
@@ -102,11 +108,17 @@ function build() {
   echo "^^^ You can safely ignore these errors. ^^^"
 }
 
-function setIdrisLibraryPath () {
-  export IDRIS_LIBRARY_PATH=$(pwd)/idris_libs
+function setupIdris () {
+  unique_libs=($(printf "%s\n" "${IDRIS_I_PATHS[@]}" | sort | uniq))
+  idris_paths=()
+  for path in "${unique_libs[@]}"; do
+    idris_paths+=("-i $path")
+  done
+  idris_paths+=("-i $(pwd)/idris_libs")
+  alias idris="idris ${idris_paths[@]}"
 }
 
 function run() {
-  setIdrisLibraryPath
+  setupIdris
   /run/current-system/sw/bin/$1 $2
 }
