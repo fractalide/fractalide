ec593547a42f99e292681d6837bd7a396caa3611
Stewart Mackenzie <setori88@gmail.com>
2018-02-21 06:05:57 +0800

    Problem: directory structure incorrect
    Solution: the structure should make space for the rest of
    the application, hence the current code should occupy a directory
    suitable for its narrow purpose.

--- pkgs/fractalide/fractalide.rkt
+++
@@ -1,8 +0,0 @@
-#lang typed/racket
-
-(provide build-fractal build-node build-subgraph build-edge)
-
-(require "fractal/fractal-maker.rkt")
-(require "node/node-maker.rkt")
-(require "subgraph/subgraph-maker.rkt")
-(require "edge/edge-maker.rkt")
---
+++ pkgs/hyperflow/fractal-management/default.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(provide build-fractal build-node build-subgraph build-edge)
+
+(require "fractal/default.rkt")
+(require "node/default.rkt")
+(require "subgraph/default.rkt")
+(require "edge/default.rkt")
