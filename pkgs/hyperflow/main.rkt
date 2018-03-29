22a38c9eb78b62925a80bdad5ab1c906eb8e2e9f
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-03-29 23:41:17 +0800

    Problem: fractalide.png is relative to the current directory
    The splash will only be found if hyperflow is started from within pkgs/hyperflow.
    
    Solution: Use a path relative to the main.rkt file.

--- pkgs/hyperflow/main.rkt
+++ pkgs/hyperflow/main.rkt
@@ -11,9 +11,12 @@
 
 (module+ main
   (require framework/splash)
+  (require racket/runtime-path)
+
+  (define-runtime-path splash-image "imgs/fractalide.png")
 
   (set-splash-progress-bar?! #t)
-  (start-splash "imgs/fractalide.png" " " 0)
+  (start-splash splash-image " " 0)
   (main)
   (shutdown-splash)
   (close-splash))
