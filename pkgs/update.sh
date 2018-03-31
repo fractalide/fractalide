97b79333f4b66610835d94ae219a1d2abca2b3b5
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-03-31 10:46:52 +0800

    Problem: pkgs/update.sh is cwd-sensitive
    Solution: Make it cd to the containing directory
    
    Now you can run ./pkgs/update.sh and get fractalide.nix in the right
    place.

--- pkgs/update.sh
+++ pkgs/update.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+SCRIPT_NAME=${BASH_SOURCE[0]##*/}
+cd "${BASH_SOURCE[0]%${SCRIPT_NAME}}"
+
 nix-shell ../../racket2nix -A racket2nix-env --run "racket2nix ../../fractalide > fractalide.nix"
