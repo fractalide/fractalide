7294027871e9711c098d26547926f4c04037af4e
Michiels Denis <dmichiels@mailoo.org>
2018-04-17 10:43:43 +0200

    Problem : agent doesn't support array port
    solution : add the fields and the necessary methods.
    Also, I add a little helper method for simple port.

--- modules/rkt/rkt-fbp/agents/adder.rkt
+++ modules/rkt/rkt-fbp/agents/adder.rkt
@@ -5,7 +5,11 @@
 (require fractalide/modules/rkt/rkt-fbp/agent)
 (require fractalide/modules/rkt/rkt-fbp/port)
 
-(define agt (opt-agent '("in") '("out")
-                         (lambda (self)
-                           (let ([msg (recv self "in")])
-                             (send self "out" (+ msg 10))))))
+(define agt (opt-agent
+             '("in") ; in port
+             '() ; in array port
+             '("out") ; out port
+             '() ; out array port
+             (lambda (self)
+               (let ([msg (recv self "in")])
+                 (send self "out" (+ msg 10))))))
--- modules/rkt/rkt-fbp/agents/displayer.rkt
+++ modules/rkt/rkt-fbp/agents/displayer.rkt
@@ -5,7 +5,11 @@
 (require fractalide/modules/rkt/rkt-fbp/agent)
 (require fractalide/modules/rkt/rkt-fbp/port)
 
-(define agt (opt-agent '("in") '()
-                             (lambda (self)
-                               (display "msg received: ")
-                               (displayln (recv self "in")))))
+(define agt (opt-agent
+             '("in") ; in port
+             '() ; in array port
+             '() ; out port
+             '() ; out array port
+             (lambda (self)
+               (display "msg received: ")
+               (displayln (recv self "in")))))
