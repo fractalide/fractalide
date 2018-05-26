1659feb4dee6ba0cf1ea364ff2034c363290ed7e
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-05-26 22:34:16 +0200

    Problem: packages on fracta hydra are not signed
    Solution: Do not check signatures (for now)

--- support/utils/setup-hydra.fractalide.com.sh
+++ support/utils/setup-hydra.fractalide.com.sh
@@ -15,11 +15,12 @@ nixos_key="cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 mkdir -p $(dirname $nix_conf)
 touch $nix_conf
 {
-  grep -vE 'substituters|public-keys' $nix_conf || true
+  grep -vE 'substituters|public-keys|require-sigs' $nix_conf || true
   cat <<EOF
 substituters = $nixos_url $fracta_url
 trusted-substituters = $nixos_url $fracta_url
 trusted-public-keys = $nixos_key $fracta_key
+require-sigs = false
 EOF
 } > ${nix_conf}.new
 mv ${nix_conf}.new $nix_conf
