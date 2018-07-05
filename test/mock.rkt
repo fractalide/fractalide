e57a928760f07de6f604a5c7c459acdfc3fcffd0
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-07-05 13:33:53 +0200

    Problem: paging-jsend-get: No test
    Solution: Create test.
    
    Cannot get it to run, due to fractalide/fractalide#246 .

---
+++ test/mock.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(require json)
+
+(require fractalide/modules/rkt/rkt-fbp/agent)
+
+(require paging-jsend-get)
+
+(provide agt)
+
+(define agt
+  (define-agent
+    #:input '("in" "jsend")
+    #:output '("out" "state")
+    #:proc
+    (lambda (input output input-array output-array)
+      (define state (make-hash))
+      (define request (recv (input "in")))
+      (match-define (http-get-request url _) request)
+      (hash-set! state 'request request)
+      (send (output "out") (jsexpr->string #hash(
+        ("status" . "success")
+        ("data" . (list "hello"))
+        ("meta" . #hash(("pagination" . #hash(
+          ("page" . 1)
+          ("perPage" . 1)
+          ("totalPages" . 1)
+          ("totalEntries" . 1))))))))
+      (define jsend-response (list (recv (input "jsend")) (recv (input "jsend"))))
+      (hash-set! state 'jsend-response jsend-response)
+      (send (output "state") state))))
