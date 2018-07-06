9c30c099247cd1851020dc1fcf6772a52c171ec9
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-07-06 19:55:20 +0800

    Problem: nodes: No catalog.rktd
    Solution: Add script for generating catalog from package. Generate it.
    
    Also clean up dependencies.
    
    Fractalide is not a dependency. Only the test requires it. We
    don't have a good mechanism in place for that situation yet, and
    we don't want a circular dependency.
    
    The test module should load everything fractalide dynamically, so
    that the package compiles without fractalide.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "paging-jsend-get")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("rackunit-lib"))
 (define scribblings '())
 (define pkg-desc "Generic node that makes GET requests and expects paginated JSend replies a la Cardano.")
 (define version "0.0")
