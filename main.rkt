b827970323a80f02d355e0cd3ec1039d3ed45f8e
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-07-06 19:43:34 +0800

    Problem: paging-jsend-get: Test is not in the idiomatic place
    Solution: Instead of calling test.rkt from main.rkt/test, just move it
    inside.

--- main.rkt
+++ main.rkt
@@ -102,5 +102,34 @@
       (do-get-request (string->url url) accept))))
 
 (module+ test
-  (require syntax/location)
-  (dynamic-require (quote (submod paging-jsend-get/test main)) #f))
+  (require rackunit)
+  (require fractalide/modules/rkt/rkt-fbp/def)
+  (require fractalide/modules/rkt/rkt-fbp/port)
+  (require fractalide/modules/rkt/rkt-fbp/scheduler)
+
+    (test-case
+      "Trivial HTTP request returning 1 item only"
+      (define sched (make-scheduler #f))
+      (define state-port (make-port 30 #f #f #f))
+      (for ([msg (list (msg-add-agent "jsend" 'paging-jsend-get)
+                       (msg-add-agent "mock" 'paging-jsend-get/test/mock)
+                       (msg-connect "jsend" "send-request" "mock" "in")
+                       (msg-connect "mock" "out" "jsend" "recv-response")
+                       (msg-connect "jsend" "send-response" "mock" "jsend")
+                       (msg-raw-connect "mock" "state" state-port))])
+        (sched msg))
+
+      (define url "http://example.com/api/hello")
+
+      (sched (msg-mesg "jsend" "recv-request" (http-get-request url #f)))
+      (define state (port-recv state-port))
+      (sched (msg-stop))
+
+      (check-true (hash-has-key? state 'request) "got http request")
+      (match-define (http-get-request got-url _) (hash-ref state 'request))
+      (check-equal? got-url url "got correct url")
+      (check-true (hash-has-key? state 'jsend-response) "got jsend response")
+      (match-define (list response1 response2) (hash-ref state 'jsend-response))
+      (match-define (json-data response1-data) response1)
+      (check-equal? response1-data "hello world" "got the unwrapped message")
+      (check-equal? response2 eof "got eof after message")))
