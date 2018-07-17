e49c994239b8229adcaa04c723adcac05d79ad23
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-07-17 17:46:42 +0800

    Problem: Code redundancy in test/main
    Some agents are just special cases of option-transform, two are unused.
    
    Solution: Refactor, remove.
    
    test/main: Add main submodule, to not have to remember the fvm command line.

--- modules/rkt/rkt-fbp/agents/test/to-close.rkt
+++ modules/rkt/rkt-fbp/agents/test/to-close.rkt
@@ -1,10 +1,9 @@
 #lang racket/base
 
-(require fractalide/modules/rkt/rkt-fbp/agent)
+(require fractalide/modules/rkt/rkt-fbp/graph)
 
-(define-agent
-  #:input '("in")
-  #:output '("out")
-  (fun
-    (let* ([msg (recv (input "in"))])
-      (send (output "out") (cons 'close #t)))))
+(define-graph
+  (node "close" ${plumbing.option-transform})
+  (mesg "close" "option" (lambda (_) '(close . #t)))
+  (edge-in "in" "close" "in")
+  (edge-out "close" "out" "out"))
