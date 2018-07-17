60e3b6afc5a64438a9dc41669716b0deb418e3f3
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-07-17 18:00:07 +0800

    Problem: No mux agent
    Takes a pair on 'in', sends the cdr to the out designated by the car.
    
    The counterpart of the demux agent.
    
    Solution: Implement it.

---
+++ modules/rkt/rkt-fbp/agents/plumbing/mux.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(require fractalide/modules/rkt/rkt-fbp/agent)
+
+(define-agent
+  #:input '("in")
+  #:output-array '("out")
+  (fun
+   (define msg (recv (input "in")))
+   (send (hash-ref (output-array "out") (car msg)) (cdr msg))))
+
+(module+ test
+  (require rackunit)
+  (require syntax/location)
+
+  (require fractalide/modules/rkt/rkt-fbp/def)
+  (require fractalide/modules/rkt/rkt-fbp/port)
+  (require fractalide/modules/rkt/rkt-fbp/scheduler)
+
+  (test-case
+   "Sending message (X . Y) yields message Y on port X"
+   (define sched (make-scheduler #f))
+   (define tap (make-port 30 #f #f #f))
+
+   (define selection "the-selection")
+   (define msg "hello")
+
+   (sched (msg-add-agent "agent-under-test" (quote-module-path ".."))
+          (msg-add-agent "identity" 'plumbing/option-transform)
+          (msg-mesg "identity" "option" identity)
+
+          (msg-connect-array-to "agent-under-test" "out" selection "identity" "in")
+          (msg-raw-connect "identity" "out" tap))
+
+   (sched (msg-mesg "agent-under-test" "in" (cons selection msg)))
+   (check-equal? (port-recv tap) msg)
+   (sched (msg-stop))))
