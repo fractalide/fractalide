e5431677c05ffab9000c04c970692847d9e35530
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-07-18 14:40:41 +0800

    Problem: Too much console debug spam
    Solution: Silence debug output from 'frame' for the unhandled events we are aware of.

--- modules/rkt/rkt-fbp/agents/gui/frame/frame.rkt
+++ modules/rkt/rkt-fbp/agents/gui/frame/frame.rkt
@@ -27,5 +27,9 @@
      [(cons 'dynamic-remove graph)
       (send (output "fvm") msg)]
      [(cons 'close #t) (send (output "halt") #t) (send (output "fvm") (cons 'stop #t))]
+     [(cons (or 'motion 'leave 'enter 'left-down 'left-up 'subwindow-focus
+                'move 'superwindow-show 'size 'focus 'radio-box 'key 'list-box
+                'text-field 'check-box 'slider)
+            _) (void)]
      [else (display "msg: ") (displayln msg)])
    (send (output "acc") fr)))
