74657f426e5b37a3a8c8cd167d2b8799fdf286f9
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-07-25 12:08:05 +0800

    Problem: No ${plumbing.identity}
    Actually, using identity is pretty common in tests, and having it
    available by name is convenient.
    
    Solution: Bring it back.
    
    Don't implement it as a graph, as that wouldn't be usable in test that
    don't use fvm.

---
+++ modules/rkt/rkt-fbp/agents/plumbing/identity.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require fractalide/modules/rkt/rkt-fbp/agent)
+
+(define-agent
+  #:input '("in")
+  #:output '("out")
+  (fun
+   (send (output "out") (recv (input "in")))))
+
+(module+ test
+  (require rackunit)
+  (require syntax/location)
+
+  (require fractalide/modules/rkt/rkt-fbp/def)
+  (require fractalide/modules/rkt/rkt-fbp/port)
+  (require fractalide/modules/rkt/rkt-fbp/scheduler)
+
+  (test-case
+   "Sending message X returns message X"
+   (define sched (make-scheduler #f))
+   (define tap (make-port 30 #f #f #f))
+
+   (define msg "hello")
+
+   (sched (msg-add-agent "agent-under-test" (quote-module-path ".."))
+          (msg-raw-connect "agent-under-test" "out" tap))
+
+   (sched (msg-mesg "agent-under-test" "in" msg))
+   (check-equal? (port-recv tap) msg)
+   (sched (msg-stop))))
+
