fcadbe5c0a913a2eb2c72ac356cb34323430cfd5
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-08-06 16:21:35 +0800

    Problem: cardano-wallet: wallets-model: No test for select input
    Solution: Add test.
    
     - Add missing "select" output.
     - Unify "select" naming for input (signal from choice box) and output
       (signal to observers).

--- modules/rkt/rkt-fbp/agents/cardano-wallet/wallets-model.rkt
+++ modules/rkt/rkt-fbp/agents/cardano-wallet/wallets-model.rkt
@@ -6,35 +6,40 @@
   (define-values (heads tail) (split-at lst idx))
   (append heads (cdr tail)))
 
+(define ports '("in" "add" "select" "delete"))
+
 (define-agent
-  #:input '("in" "add" "select" "delete")
-  #:output '("out")
+  #:input ports
+  #:output '("out" "select")
   (fun
    (define acc (or (try-recv (input "acc")) (make-hash)))
-   (define action (for/or ([port '("in" "add" "delete")])
+   (define action (for/or ([port ports])
                           (define val (try-recv (input port)))
                           (if val (cons port val) #f)))
    (for ([port-msg (match action
                     [(cons "in" (list))
                      (list (cons "acc" (list))
-                           (cons "selection" 0)
+                           (cons "select" 0)
                            (cons "out" (make-hash)))]
                     [(cons "in" (list-rest head rest))
                      (list (cons "acc" (list* head rest))
-                           (cons "selection" 0)
+                           (cons "select" 0)
                            (cons "out" head))]
                     [(cons "add" wallet-data)
                      (list (cons "acc"
                                  (append acc (list wallet-data)))
-                           (cons "selection" (length acc))
+                           (cons "select" (length acc))
                            (cons "out" wallet-data))]
                     [(cons "delete" selection)
                      (define new-selection (min (max 0 (- (length acc) 2))
                                                 selection))
                      (define new-acc (list-remove-index acc selection))
                      (list (cons "acc" new-acc)
-                           (cons "selection" new-selection)
-                           (cons "out" (list-ref new-acc new-selection)))])])
+                           (cons "select" new-selection)
+                           (cons "out" (list-ref new-acc new-selection)))]
+                    [(cons "select" selection)
+                     (list (cons "acc" acc)
+                           (cons "out" (list-ref acc selection)))])])
         (match-define (cons port msg) port-msg)
         (send (output port) msg))))
 
@@ -49,7 +54,7 @@
   (test-case
    "Initializing gives us head wallet"
    (define sched (make-scheduler #f))
-   (define taps (for/hash ([port '("selection" "out")]) (values port (make-port 30 #f #f #f))))
+   (define taps (for/hash ([port '("select" "out")]) (values port (make-port 30 #f #f #f))))
 
    (sched (msg-add-agent "agent-under-test" (quote-module-path "..")))
 
@@ -60,6 +65,27 @@
                          #hash(("name" . "qwer"))))
 
    (sched (msg-mesg "agent-under-test" "in" wallets))
-   (check-equal? (port-recv (hash-ref taps "selection")) 0)
+   (check-equal? (port-recv (hash-ref taps "select")) 0)
    (check-equal? (port-recv (hash-ref taps "out")) (car wallets))
+   (sched (msg-stop)))
+
+  (test-case
+   "Initialize then select"
+   (define sched (make-scheduler #f))
+   (define taps (for/hash ([port '("select" "out")]) (values port (make-port 30 #f #f #f))))
+
+   (sched (msg-add-agent "agent-under-test" (quote-module-path "..")))
+
+   (for ([(port tap) (in-hash taps)])
+        (sched (msg-raw-connect "agent-under-test" port tap)))
+
+   (define wallets (list #hash(("name" . "asdf"))
+                         #hash(("name" . "qwer"))))
+
+   (sched (msg-mesg "agent-under-test" "in" wallets))
+   (port-recv (hash-ref taps "select"))
+   (port-recv (hash-ref taps "out"))
+
+   (sched (msg-mesg "agent-under-test" "select" 1))
+   (check-equal? (port-recv (hash-ref taps "out")) (second wallets))
    (sched (msg-stop))))
