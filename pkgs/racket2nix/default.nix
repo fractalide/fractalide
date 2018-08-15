1a9df5f34bbab348e9140b47fdedb72d5076036c
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-08-15 10:37:41 +0800

    Problem: No overrideRacketDerivation
    Solution: Bump to fractalide/racket2nix#155.

--- pkgs/racket2nix/default.nix
+++ pkgs/racket2nix/default.nix
@@ -3,8 +3,8 @@ let
   pinnedPkgs = bootPkgs.fetchFromGitHub {
     owner = "fractalide";
     repo = "racket2nix";
-    rev = "67e4cac07ddeffc51da4a29d9c3b7c7d36129d92";
-    sha256 = "055y52sns7ng5mwxvmwibngxq17ygdqf8y4vzgimygdmh94dbakm";
+    rev = "refs/pull/155/head";
+    sha256 = "08wdwz6h69sl62iccfspwac1nbhwb1nd36mks2hb0sbwi1kh954y";
   };
 in
 import pinnedPkgs
