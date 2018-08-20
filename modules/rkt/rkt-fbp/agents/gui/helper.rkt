4c0b80c7ccc72175f5d8e348c6e2cdb3cb988924
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-08-20 18:18:09 +0800

    Problem: gui.helper: Nested ifs
    Flow is difficult to follow.
    
    Solution: Replace inner ifs and begins with cond.

--- modules/rkt/rkt-fbp/agents/gui/helper.rkt
+++ modules/rkt/rkt-fbp/agents/gui/helper.rkt
@@ -229,18 +229,17 @@
   (cond
    [(and (pair? acc) (eq? (car acc) 'init))
     ; widget in creation
-    (if (eq? (car msg) 'init)
-        ; True -> create widget and receive it
-        ;      -> process the list
-        (begin
-         ; create the widget
-         (send (output "out") (cons 'init (create input (cdr msg))))
-         (let ([widget (recv (input "acc"))])
-              (for ([m (cdr acc)])
-                   (process-msg m widget input output output-array))
-              widget))
-        ; False -> add to the list
-        (cons 'init (cons msg (cdr acc))))]
+    (cond
+     [(eq? (car msg) 'init)
+      ; create widget and receive it
+      ; process the list
+      (send (output "out") (cons 'init (create input (cdr msg))))
+      (let ([widget (recv (input "acc"))])
+           (for ([m (cdr acc)])
+                (process-msg m widget input output output-array))
+           widget)]
+     [else
+      (cons 'init (cons msg (cdr acc)))])]
    [(and (cons? msg) (eq? (car msg) 'init))
     ; recreate widget
     (send (output "out") (cons 'init (create input (cdr msg))))
