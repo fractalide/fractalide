d779e6363bfd03dd5c702e1c5329fecbd0aa837e
Denis Michiels <dmichiels@mailoo.org>
2018-08-31 10:39:14 +0200

    Problem: Unable to get hyperflow to load a graph
    Solution: fix the path
    
    This is not the best solution, I will work later to use the same methods than
    the scheduler.

--- modules/rkt/rkt-fbp/agents/hyperflow/graph/in-agent.rkt
+++ modules/rkt/rkt-fbp/agents/hyperflow/graph/in-agent.rkt
@@ -7,6 +7,6 @@
   #:input '("in") ; in array port
   #:output '("out") ; out port
    (define msg (recv (input "in")))
-   (define path (string-split msg "fractalide/modules/rkt/rkt-fbp/"))
+   (define path (string-split msg "modules/rkt/rkt-fbp/"))
    (define o (g-agent "" (cadr path)))
    (send (output "out") o))
