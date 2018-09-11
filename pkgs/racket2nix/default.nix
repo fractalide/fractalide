b473d55d38ab1ba8f6a0e8719bd8db66c32629b9
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-11 18:09:30 +0800

    Problem: buildThinRacket can't handle us
    It has a workaround for fixing the pname of the given source, which
    doesn't work on store paths.
    
    Solution: Bump to fractalide/racket2nix#216

--- pkgs/racket2nix/default.nix
+++ pkgs/racket2nix/default.nix
@@ -3,8 +3,8 @@ let
   pinnedPkgs = bootPkgs.fetchFromGitHub {
     owner = "fractalide";
     repo = "racket2nix";
-    rev = "8345dbd56f0c8164646eb039427c155a51014708";
-    sha256 = "0ds08qjpcs1wlpbywbz8bhlla5d2z91137aghhm0m2wqig70rijy";
+    rev = "75f84d3708e1155323b0575f34b169d2f22ca369";
+    sha256 = "019ad4j19z2gwc4knam2sqi1ybwbyasr81yq8zahj2b2zk4g9ywq";
   };
 in
 import pinnedPkgs
