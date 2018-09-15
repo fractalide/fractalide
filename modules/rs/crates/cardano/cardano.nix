f35c3d22e95dd15f7293114b16bb3eab7d65c031
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-15 23:04:27 +0800

    Problem: cardano.nix: No bump script.
    Solution: Implement.

--- modules/rs/crates/cardano/cardano.nix
+++ modules/rs/crates/cardano/cardano.nix
@@ -1,1 +1,1 @@
-fetchgit { url = "https://github.com/input-output-hk/rust-cardano.git"; rev = "6d871ac5e2c574cfa84bbdd1200e38ea3bb0e2e3"; sha256="0x1jaf9w9zbpavsgc2z4m5ibi54r6lvyp7anaigd3dmp2j2cnm8g"; }
+fetchgit { url = "https://github.com/input-output-hk/rust-cardano.git"; rev = "6d871ac5e2c574cfa84bbdd1200e38ea3bb0e2e3"; sha256 = "0x1jaf9w9zbpavsgc2z4m5ibi54r6lvyp7anaigd3dmp2j2cnm8g"; }
