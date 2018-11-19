19876c36f270732231c6b34b6702d8898de9e1e8
Denis Michiels <dmichiels@mailoo.org>
2018-11-19 08:10:31 +0100

    Problem: cardano-wallet: no display of wallet
    Solution: implement the basic layout of a wallet screen

---
+++ modules/rkt/rkt-fbp/agents/cardano-wallet/wallet/summary.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require fractalide/modules/rkt/rkt-fbp/graph)
+
+(define-graph
+  (node "hp" ${gui.horizontal-panel})
+  (edge-out "hp" "out" "out")
+  (edge-in "in" "hp" "in")
+
+  (node "headline" ${gui.message})
+  (edge "headline" "out" _ "hp" "place" 2)
+  (mesg "headline" "in" '(init . ((label . "Summary"))))
+)
--- modules/rkt/rkt-fbp/agents/test.rkt
+++ modules/rkt/rkt-fbp/agents/test.rkt
@@ -4,6 +4,9 @@
 
 (define-graph
   (node "frame" ${gui.frame})
-  (node "wcreate" ${cardano-wallet.wcreate})
-  (edge "wcreate" "out" _ "frame" "in" _)
+  ;; (node "wcreate" ${cardano-wallet.wcreate})
+  ;; (edge "wcreate" "out" _ "frame" "in" _)
+
+  (node "wallet" ${cardano-wallet.wallet})
+  (edge "wallet" "out" _ "frame" "in" _)
   )
