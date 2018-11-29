d16cee52e5bde431d457550cd9d2f57a68407aff
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-11-30 06:38:42 +0800

    Problem: carnix no longer takes the 'nix' command
    Solution: Change to 'generate-nix'.
    
     - Run update.sh.
    
    Apparently the generated Cargo.nix is now much smaller, and piggybacks
    off crates-io.nix in nixpkgs.

--- modules/rs/crates/update.sh
+++ modules/rs/crates/update.sh
@@ -7,6 +7,6 @@ set -u
 nix-build Cargo.toml.nix | xargs cat > Cargo.toml.new
 mv Cargo.toml{.new,}
 cargo generate-lockfile
-carnix nix --src ./. 
+carnix generate-nix --src ./.
 nix-build Cargo.nix.nix | xargs cat > Cargo.nix.new
 mv Cargo.nix{.new,}
